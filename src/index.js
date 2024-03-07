177c4a338be4e10619f41b01ba5748d5036ee707
Bhupendra <bhupendranasare@gmail.com>
2024-03-08 00:44:31 +0530

    Completed

--- src/index.js
+++ src/index.js
@@ -4,17 +4,19 @@ const mongoose = require('mongoose');
 const app = express();
 const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
+const cors = require("cors");
 //connect to mongoDB
 const MONGODB_URI = process.env.MONGODB_URI
+const PORT = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json())
 app.use("/api/auth",authRoutes);
-app.use("/api/task",taskRoutes);
+app.use("/api/tasks",taskRoutes);
 mongoose.connect(MONGODB_URI).then(()=>console.log('Connected to MongoDB')).catch((err)=>console.log(err))
 app.get("/",(req,res)=>{
     res.send("Backend Server is running")
 })
-const PORT = process.env.PORT || 5000;
 
 app.listen(5000,()=>{
     console.log('Server is running on port 5000');
